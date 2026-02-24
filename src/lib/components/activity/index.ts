ae012745a67daa4ddcecbfff715684cd1ffbbef5
Alastair Dawson <283834+kreek@users.noreply.github.com>
2026-02-24 14:14:41 -0800

    Remove unused components and icons

--- src/lib/components/activity/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as ActivityNav } from './ActivityNav.svelte';
--- src/lib/components/icons/index.ts
+++ src/lib/components/icons/index.ts
@@ -1,12 +1,9 @@
 export { default as BookIcon } from './BookIcon.svelte';
 export { default as CheckIcon } from './CheckIcon.svelte';
-export { default as CloseIcon } from './CloseIcon.svelte';
 export { default as GroupIcon } from './GroupIcon.svelte';
 export { default as PlusIcon } from './PlusIcon.svelte';
-export { default as ProjectIcon } from './ProjectIcon.svelte';
 export { default as RobotIcon } from './RobotIcon.svelte';
 export { default as SearchIcon } from './SearchIcon.svelte';
 export { default as SettingsIcon } from './SettingsIcon.svelte';
 export { default as StateIcon } from './StateIcon.svelte';
 export { default as TrashIcon } from './TrashIcon.svelte';
-export { default as WandSparklesIcon } from './WandSparklesIcon.svelte';
